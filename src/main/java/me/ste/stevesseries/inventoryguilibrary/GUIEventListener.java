e530c3184f557697dc8cc646ba755d07ca223825
SteveTheEngineer <sthesengineer@gmail.com>
2020-11-16 16:35:13 +0400

    Fix items being able to be dragged and put inside GUIs

--- src/main/java/me/ste/stevesseries/inventoryguilibrary/GUIEventListener.java
+++ src/main/java/me/ste/stevesseries/inventoryguilibrary/GUIEventListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.PlayerInventory;
@@ -32,6 +33,15 @@ public class GUIEventListener implements Listener {
             if(InventoryGUILibrary.getInstance().getPlayerGuis().containsKey(event.getWhoClicked().getUniqueId()) && (!(event.getClickedInventory() instanceof PlayerInventory) || event.getClick().isShiftClick())) {
                 event.setCancelled(true);
                 InventoryGUILibrary.getInstance().getPlayerGuis().get(event.getWhoClicked().getUniqueId()).handleClick(event.getCurrentItem(), event.getClick(), event.getSlot(), event.getClickedInventory());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent event) {
+        if(event.getWhoClicked() instanceof Player) {
+            if(InventoryGUILibrary.getInstance().getPlayerGuis().containsKey(event.getWhoClicked().getUniqueId()) && !(event.getInventory() instanceof PlayerInventory)) {
+                event.setCancelled(true);
             }
         }
     }
